1799091c3cd82c6132c984d28006ea58be269ede
Noel <nisraeltedros@gmail.com>
2026-07-19 03:46:26 +0300

    i just used useState it iz for causing re render every time i call a functio

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import Message from "./Message.tsx";
+//import Message from "./Message.tsx";
+import { useState } from "react";
 function App() {
-  
+  const [num, numSetter] = useState(0)
   return (
     <div>
-      <Message></Message>
+      <h2>the number is, {num}</h2>
+      <button onClick={()=>numSetter((num)=> num+1)}>counter</button>
     </div>
   );
 }
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,18 +1,9 @@
 function Message() {
-  const myName = "";
-  if (myName) {
-    return (
-      <>
-        <h1>Hello {myName}</h1>
-      </>
-    );
-  } else {
-    return (
-        <>
-        <h1>Hello World</h1>
-        </>
-    );
-  }
+  return (
+    <>
+      <h1>Hello World</h1>
+    </>
+  );
 }
 
 export default Message;
