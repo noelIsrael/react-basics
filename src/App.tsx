03a72a2031bbb8fafcd73a744248c868da38ae45
Noel <nisraeltedros@gmail.com>
2026-07-19 16:26:31 +0300

    just importing components / passing props from copm to comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import Message from "./Message.tsx";
 import { useState } from "react";
 function App() {
-  const [num, numSetter] = useState(0)
+  const [num, numSetter] = useState(0);
+  console.log("comp app renderer")
+  
   return (
     <div>
       <h2>the number is, {num}</h2>
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,16 +1,22 @@
 import { useState } from "react";
 
-function Greeting (props:{shim :string}){
-    return (
-        <h1>Hello {props.shim}</h1>
-    )
+function Greeting(props: { shim: string; idme: number }) {
+  return (
+    <>
+      <h1>Hello {props.shim}</h1>
+      <h1>yor age is {props.idme}</h1>
+    </>
+  );
 }
 function Message() {
   const [name, setName] = useState("");
+  const [age, setAge]= useState(0);
+  console.log("comp message rendered")
   return (
     <>
-      <input onChange={(x)=>setName(x.target.value)}/>
-      <Greeting shim={name}/>
+      <input onChange={(x) => setName(x.target.value)} />
+      <input onChange={(y)=> setAge(Number(y.target.value))}/>
+      <Greeting shim={name} idme={age} />
     </>
   );
 }
