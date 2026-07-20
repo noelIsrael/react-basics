1dd26eff576b4afc743ab4d52f25f1f79a3121a0
Noel <nisraeltedros@gmail.com>
2026-07-20 17:52:36 +0300

    functions go up states go down

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-// import Message from "./Message.tsx";
-// import { useState } from "react";
-function Shower(props: { name?: string; age: number }) {
-  return (
-    <h4>
-      hi {props.name} you are {props.age}
-    </h4>
-  );
+import Areylen from "./areylen.tsx";
+import { useState } from "react";
+function Display (props : {name:string}){
+  return(
+    <h4 >hi {props.name} </h4>
+  )
 }
-function App() {
-  const noni = { name: "noel", age: 22 };
-  const abi = { name: "abi", age: 23 };
-  const friends = [noni, abi];
-  // const me = "noh";
+function New ({sebwesk} : {sebwesk : (name:string , id:number)=> void}){
+  const [name, nameSet] =useState("");
+  const [id, idSet] =useState("");
+  return(
+    <>
+    <input placeholder="enter name" onChange={(e)=>nameSet(e.target.value)}/>
+    <input placeholder="enter id" onChange={(e)=>idSet(e.target.value)}/>
+    <button onClick={()=>sebwesk(name,Number(id))}>submit</button>
+    </>
+  )
+}
 
-  return (
-    <div>
-      {friends
-        .filter((fri) => fri.age > 22)
-        .map((friend) => (
-          <Shower name={friend.name} age={friend.age} />
-        ))}
-    </div>
+
+function App() {
+  const noel = {id: 1, name:'noni'};
+  const abel = {id:2, name:'abi'};
+  const sada = {id:3, name:'sadu'};
+
+
+  const [arey, areySeter] = useState([noel,abel,sada])
+  return(
+    <>
+    {arey.map((human)=> (<Display key={human.id} name={human.name} />))}
+    <New sebwesk={(name,id)=>areySeter([...arey,{name:name,id:id} ])}/>
+    <Areylen bzhi={arey.length} />
+    </>
   );
 }
 export default App;
---
+++ src/areylen.tsx
@@ -0,0 +1,8 @@
+function Areylen (props: {bzhi : number}){
+    return(
+        <>
+        <h2>the total number of users is {props.bzhi}</h2>
+        </>
+    )
+}
+export default Areylen
